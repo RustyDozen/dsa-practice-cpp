f605f906495daf2f70cbd51f14982004ce333da7
RustyDozen <rustydozensocial@gmail.com>
2025-09-17 23:54:37 +0530

    Add Sum of Two Arrays problem solution

---
+++ sum-of-two-arrays.cpp
@@ -0,0 +1,44 @@
+vector<int> reverse(vector<int> v) {
+	int s = 0;
+	int e = v.size() - 1;
+	while (s <= e) {
+		swap(v[s++], v[e--]);
+	}
+	return v;
+}
+#include <bits/stdc++.h> 
+vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m) {
+	int i = n - 1;
+	int j = m - 1;
+	int carry = 0;
+	vector<int> ans;
+	while (i >= 0 && j >= 0) {
+		int sum = a[i] + b[j] + carry;
+		carry = sum / 10;
+		int value = sum % 10;
+		ans.push_back(value);
+		i--;
+		j--;
+	}
+	while (i >= 0) {
+		int sum = a[i] + carry;
+		carry = sum / 10;
+		int value = sum % 10;
+		ans.push_back(value);
+		i--;
+	}
+	while (j >= 0) {
+		int sum = b[j] + carry;
+		carry = sum / 10;
+		int value = sum % 10;
+		ans.push_back(value);
+		j--;
+	}
+	while (carry != 0) {
+		int sum = carry;
+		carry = sum / 10;
+		int value = sum % 10;
+		ans.push_back(value);
+	}
+	return reverse(ans);
+}
